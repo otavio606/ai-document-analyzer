accf42cef1f52ba0221f791e1896c5c1917f47e0
otavio606 <otaviojorge11@gmail.com>
2026-05-20 21:06:55 -0300

    feat: add frontend

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 app.use("/api", require("./routes/analyze"));
 
-// Rota de teste
 app.get("/", (req, res) => {
-  res.json({ mensagem: "Servidor do Document Analyzer está online! 🚀" });
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.listen(PORT, () => {
